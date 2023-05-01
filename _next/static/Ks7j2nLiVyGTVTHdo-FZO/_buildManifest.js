e58e12ff0cbb6ff3a9d8585dea5ae17ddfea0314
hamidre13 <hamid@seodapop.com>
2023-05-01 22:35:24 +0000

    Deploying to gh-pages from @ seodapop/seodapop-main-website@b83be542e63d6294477a359083b3789241b36cd2 🚀

--- _next/static/Ks7j2nLiVyGTVTHdo-FZO/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/_error":["static/chunks/pages/_error-942918685354aae9.js"],"/plasmic-host":["static/chunks/983-1556a0cc06a46a83.js","static/chunks/pages/plasmic-host-db9103a01a045bb6.js"],sortedPages:["/_app","/_error","/plasmic-host"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
